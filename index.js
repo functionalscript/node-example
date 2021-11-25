2a8851c99ac210ebd70c87e55a2575bb8d5b3c2a
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-11-24 17:09:54 -0800

    no multiply

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 module.exports = {
-    /** @type {(x: number) => (y: number) => number} */
-    "multiply": x => y => x * y, 
-    json: {
-        "a": 0,
-        "4294967294": 1,
-        "4294967295": 2,        
-    }
+    "a": 0,
+    "4294967294": 1,
+    "4294967295": 2,
 }
