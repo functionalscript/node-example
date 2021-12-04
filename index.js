4ec61b11cc06b12e375244a7bf1ffbaad804c40e
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-12-03 23:38:16 -0800

    42

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
     "4294967294.0": 0,
     "4294967294": 1,
-    "4294967295": 2,
+    "4294967295": 42,
     /** @type {(a:number) => (b: number) => number} */
     multiply: a => b => a * b
 }
